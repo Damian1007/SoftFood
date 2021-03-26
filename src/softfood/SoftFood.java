3c990c02b34aebd1fb61cb0043df8db117e82a1d
Damian <Damian@DESKTOP-6C7MSN1>
2021-03-26 11:45:27 -0500

    Merge origin/master
    Conflicts:
    	src/main/java/com/mycompany/softfood/Ejemplo.java

---
+++ src/softfood/SoftFood.java
@@ -0,0 +1,16 @@
+package softfood;
+
+/**
+ *
+ * @author Damian
+ */
+public class SoftFood {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
